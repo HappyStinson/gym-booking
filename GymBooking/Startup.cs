3ec2a65402c63b792bb101dc2673d8f927ed7bec
Rasmus Nordling <rasmus.nordling@gmail.com>
2017-03-14 09:59:23 +0100

    Add project files.

---
+++ GymBooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymBooking.Startup))]
+namespace GymBooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
